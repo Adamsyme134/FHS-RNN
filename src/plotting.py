fd4af8971a8e023510913768f14477e8cbc5c338
Adam Syme <adamsyme@Adams-MacBook-Air-2.local>
2026-05-13 13:12:55 +0100

    plotting for task generator

---
+++ src/plotting.py
@@ -0,0 +1,44 @@
+from task import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_task_batch():
+    padded_inputs, padded_targets, lengths, mask = generate_batch(5)
+
+    fig, axes = plt.subplots(5, 1, figsize=(10, 8))
+
+    for i in range(5):
+
+        # Plot input matrix
+        axes[i].imshow(
+            padded_inputs[i].numpy().T,
+            aspect='auto',
+            cmap='Greys',
+            interpolation='nearest'
+        )
+
+        # Get target sequence
+        target = padded_targets[i].numpy()
+
+        # Time axis
+        x = np.arange(len(target))
+
+        # Map lick rate values onto vertical positions
+        # 0 -> bottom, 1 -> top
+        y = 3 - (target * 3)
+
+        # Overlay in red
+        axes[i].plot(
+            x,
+            y,
+            color='red',
+            linewidth=2
+        )
+
+        axes[i].set_yticks([0,1,2,3])
+        axes[i].set_yticklabels(['A','B','C','R'])
+
+    plt.tight_layout()
+    plt.show()
